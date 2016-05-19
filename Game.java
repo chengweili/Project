650f8c30768b89d9d2b392e96bf5288aec2c236a
chengweili <chengweili42@gmail.com>
2016-05-19 08:35:46 -0400

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Game extends JFrame{
+
+    public Game(){
+	setTitle("Game");
+	setSize(1000,600);
+	setLocation(250,250);
+    }
+
+    public static void main(String[]args){
+	JFrame f = new Game();
+	f.show();
+    }
+
+}
